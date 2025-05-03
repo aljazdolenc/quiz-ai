1b1d7e9102b1e3941d709e6aba2eae161a5e6401
aljazdolenc <aljaz.dolenc@dhimahi.com>
2025-05-03 22:46:04 +0200

    Updated chat size

--- src/modules/chat/components/ChatInput.tsx
+++ src/modules/chat/components/ChatInput.tsx
@@ -11,7 +11,7 @@ const ChatInput = forwardRef<HTMLTextAreaElement, ChatInputProps>(({className, .
         ref={ref}
         name="message"
         className={cn(
-            "px-4 py-4 max-h-40 min-h-none text-lg bg-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md flex items-center resize-none",
+            "px-4 py-3 max-h-40 min-h-none text-lg bg-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md flex items-center resize-none",
             className,
         )}
         {...props}
